4599622a9ab3cfef2add2cdba33f31d3e4e4c30d
Yousuf Nizami <123084557+YOUSUF-NIZAMI@users.noreply.github.com>
2025-03-27 02:12:43 +0600

    Update REX.py

--- REX.py
+++ REX.py
@@ -1,27 +1,22 @@
-#<\>!python3.11
-#<\>coded by YousuF
-#----------------Don't Copy This Script--------------#
-import os, sys, platform
+import os
+import sys
+import platform
+import webbrowser
 try:
     import requests
-except:
-    os.system('pip install requests')
-os.system('xdg-open https://facebook.com/groups/1379588029581028/')
-import requests
-try:
-    if sys.argv[1]=='update':
-        os.system('rm -rf YM.so')
-except:
-    pass
-os.system('rm -rf YM.so')
-os.system('git pull')
-
+except ImportError:
+    os.system("pip install requests")
+    import requests
+webbrowser.open("https://facebook.com/groups/672149575467753/")
+os.system("git pull")
 bit = platform.architecture()[0]
-if bit == '64bit':
-    if not os.path.isfile('YM.so'):
-        os.system('curl -L https://github.com/YOUSUF-NIZAMI/FILExRANDOM/blob/main/ym.cpython-311.so?raw=true -o YM.so') 
-        import YM  
-    else:
-        import YM
-elif bit == '32bit':
-    exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+file_url = "https://github.com/YOUSUF-NIZAMI/FILExRANDOM/blob/main/main.cpython-311.so?raw=true"
+file_name = "main.cpython-311.so"
+if not os.path.isfile(file_name):
+    os.system(f"curl -L {file_url} -o {file_name}")
+
+try:
+    import main 
+except ImportError:
+    print("Error: Something went wrong while loading the required files.")
+   
